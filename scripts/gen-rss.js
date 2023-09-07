5e5feeecb6ede4ad9431cb42105c458fe5666e33
Minsu Kang <powerful@postech.ac.kr>
2023-09-07 11:48:52 +0900

    기본정보 작성

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Minsu Kang',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
